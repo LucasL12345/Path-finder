71523d12e00f6a5632639e001082b3dc82c32982
Lucas <lucaslemoine49@gmail.com>
2023-06-06 18:24:13 +0100

    added dijkstra implementation and the visualisation functionality

--- dijkstra/src/App.js
+++ dijkstra/src/App.js
@@ -20,15 +20,15 @@ function Node({ row, col, isWall, isStart, isEnd, onMouseDown, onMouseEnter, onM
 function App() {
   const [grid, setGrid] = useState(createGrid());
   const [isMousePressed, setIsMousePressed] = useState(false);
-  const [startNode, setStartNode] = useState({ row: 7, col: 5 });
-  const [endNode, setEndNode] = useState({ row: 7, col: 25 });
+  const [startPos, setStartPos] = useState({ row: 7, col: 5 });
+  const [endPos, setEndPos] = useState({ row: 7, col: 25 });
 
   function createGrid() {
     const grid = [];
     for (let row = 0; row < ROWS; row++) {
       const currentRow = [];
       for (let col = 0; col < COLS; col++) {
-        currentRow.push({ row, col, isStart: false, isEnd: false, isWall: false });
+        currentRow.push({ row, col, isStart: false, isEnd: false, isWall: false, distance: Infinity, isVisited: false, previousNode: null });
       }
       grid.push(currentRow);
     }
@@ -61,11 +61,114 @@ function App() {
     setGrid(createGrid());
   }
 
+  function visualizeDijkstra() {
+    const visitedNodesInOrder = dijkstra();
+    const shortestPath = getShortestPath();
+    animateAlgorithm(visitedNodesInOrder, shortestPath);
+  }
+
+  function dijkstra() {
+    const startNode = grid[startPos.row][startPos.col];
+    const endNode = grid[endPos.row][endPos.col];
+    const visitedNodesInOrder = [];
+    startNode.distance = 0;
+    const unvisitedNodes = getAllNodes();
+
+    while (unvisitedNodes.length > 0) {
+      sortNodesByDistance(unvisitedNodes);
+      const closestNode = unvisitedNodes.shift();
+
+      if (closestNode.isWall) continue;
+
+      if (closestNode.distance === Infinity) return visitedNodesInOrder;
+
+      closestNode.isVisited = true;
+      visitedNodesInOrder.push(closestNode);
+
+      if (closestNode === endNode) {
+        return visitedNodesInOrder;
+      }
+
+      updateUnvisitedNeighbors(closestNode);
+    }
+  }
+
+  function sortNodesByDistance(nodes) {
+    nodes.sort((a, b) => a.distance - b.distance);
+  }
+
+  function getAllNodes() {
+    const nodes = [];
+    for (let row = 0; row < ROWS; row++) {
+      for (let col = 0; col < COLS; col++) {
+        nodes.push(grid[row][col]);
+      }
+    }
+    return nodes;
+  }
+
+  function updateUnvisitedNeighbors(node) {
+    const neighbors = getNeighbors(node);
+    for (let neighbor of neighbors) {
+      const distance = node.distance + 1;
+      if (distance < neighbor.distance) {
+        neighbor.distance = distance;
+        neighbor.previousNode = node;
+      }
+    }
+  }
+
+  function getNeighbors(node) {
+    const neighbors = [];
+    const { row, col } = node;
+
+    if (row > 0) neighbors.push(grid[row - 1][col]);
+    if (row < ROWS - 1) neighbors.push(grid[row + 1][col]);
+    if (col > 0) neighbors.push(grid[row][col - 1]);
+    if (col < COLS - 1) neighbors.push(grid[row][col + 1]);
+
+    return neighbors.filter((neighbor) => !neighbor.isVisited);
+  }
+
+  function getShortestPath() {
+    const shortestPath = [];
+    let currentNode = grid[endPos.row][endPos.col];
+    while (currentNode !== null) {
+      shortestPath.unshift(currentNode);
+      currentNode = currentNode.previousNode;
+    }
+    return shortestPath;
+  }
+
+  function animateAlgorithm(visitedNodesInOrder, shortestPath) {
+    for (let i = 0; i <= visitedNodesInOrder.length; i++) {
+      if (i === visitedNodesInOrder.length) {
+        setTimeout(() => {
+          animateShortestPath(shortestPath);
+        }, 10 * i);
+        return;
+      }
+      setTimeout(() => {
+        const node = visitedNodesInOrder[i];
+        document.getElementById(`node-${node.row}-${node.col}`).className = 'node node-visited';
+      }, 10 * i);
+    }
+  }
+
+  function animateShortestPath(shortestPath) {
+    for (let i = 0; i < shortestPath.length; i++) {
+      setTimeout(() => {
+        const node = shortestPath[i];
+        document.getElementById(`node-${node.row}-${node.col}`).className = 'node node-shortest-path';
+      }, 50 * i);
+    }
+  }
+
   return (
     <div className="App">
       <div className="tab">
         <button onClick={clearBoard}>Clear Board</button>
-        <button>Start</button>
+        <button onClick={visualizeDijkstra}>Start</button>
       </div>
       <div className="grid">
         {grid.map((row, rowIndex) => (
@@ -76,8 +179,8 @@ function App() {
                 row={node.row}
                 col={node.col}
                 isWall={node.isWall}
-                isStart={node.row === startNode.row && node.col === startNode.col}
-                isEnd={node.row === endNode.row && node.col === endNode.col}
+                isStart={node.row === startPos.row && node.col === startPos.col}
+                isEnd={node.row === endPos.row && node.col === endPos.col}
                 onMouseDown={handleMouseDown}
                 onMouseEnter={handleMouseEnter}
                 onMouseUp={handleMouseUp}
